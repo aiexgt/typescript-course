64d40d918b494ff3755f63b635aff77fd9199865
aiexgt <lima14florian14@gmail.com>
2022-09-28 15:31:30 -0600

    Inicio curso

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+let hp: number | string = 165;
+let estaVivo: boolean = false;
+
+hp = 'FULL';
+
+console.log(nombre, hp, estaVivo);
